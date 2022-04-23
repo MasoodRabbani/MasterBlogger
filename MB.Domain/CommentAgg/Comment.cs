66ed9e2cbbd72b2a1cd0424afc39dc09a7ef4f60
Masood <mr.rabbani1380@gmail.com>
2022-04-23 21:52:39 +0430

    Domain Implemented Application Realted in Created

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MB.Domain.ArticleAgg;
+
+namespace MB.Domain.CommentAgg
+{
+    public class Comment
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Massage { get; private set; }
+        public int Status { get; private set; }//new =0 , confirm=1 , cancel=2
+        public DateTime CreationDate { get; private set; }
+        public long ArticleId { get; private set; }
+        public Article Article { get; private set; }
+
+        public Comment(string name, string email, string massage, long articleId)
+        {
+            Name = name;
+            Email = email;
+            Massage = massage;
+            ArticleId = articleId;
+            CreationDate=DateTime.Now;
+            Status = Statuses.New;
+        }
+
+        protected Comment()
+        {
+            
+        }
+    }
+}
